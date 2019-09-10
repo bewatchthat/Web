99f520af8adafbee0dc23b6d63c7b1c7ea925825
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2019-09-10 22:36:40 +0100

    Removed unused history util

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import styles from './App.module.scss';
 import configureStore from './redux/configure-store';
-import history from './utils/history';
 import AddView from './views/AddView';
 import HomeView from './views/HomeView';
 
@@ -13,10 +12,10 @@ function App() {
   return (
     <main className={ styles.app }>
       <Provider store={ store }>
-        <Router history={ history }>
+        <BrowserRouter>
           <Route path="/" exact component={ HomeView }/>
           <Route path="/add" component={ AddView }/>
-        </Router>
+        </BrowserRouter>
       </Provider>
     </main>
   );
